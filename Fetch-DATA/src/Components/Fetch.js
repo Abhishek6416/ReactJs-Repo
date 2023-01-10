8c91b6c062311ec4646a5d5e37dbe32403887863
Abhishek6416 <abhishekkr6416@gmail.com>
2023-01-10 14:16:54 +0530

    fetch:BasicApi

---
+++ Fetch-DATA/src/Components/Fetch.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useState } from 'react'
+
+export default function Fetch() {
+    const [state,setState]=useState([])
+
+
+    const handleEvent=()=>{
+
+
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then((response)=>response.json())
+        .then((data)=>
+        
+        setState(data))
+    }
+  return (
+    <div>
+
+
+
+        <button onClick={handleEvent}>get data</button>
+
+        {
+            state.map((elem)=>{
+
+                return(
+
+                    <div>
+
+                        <h2>{state.name}</h2>
+                    </div>
+                )
+            })
+        }
+    </div>
+  )
+}
