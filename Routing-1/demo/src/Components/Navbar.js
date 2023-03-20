110ef0ad1c38022a785ec93d0bf5bec668d576bd
Abhishek6416 <abhishekkr6416@gmail.com>
2023-03-20 16:12:50 +0530

    nav

---
+++ Routing-1/demo/src/Components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Navbar.css"
+
+export default function Navbar() {
+  return (
+    <div className='Nav'>
+
+        <a href="">Home</a>
+        <a href="">Profile</a>
+        <a href="">Todo</a>
+        <a href="">Products</a>
+        <a href="">Counter</a>
+    </div>
+  )
+}
+
+
