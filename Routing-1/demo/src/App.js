c1e67c4aca7af35e07388d872afcb881379408a2
Abhishek6416 <abhishekkr6416@gmail.com>
2023-03-22 09:35:53 +0530

    changes

--- Routing-1/demo/src/App.js
+++ Routing-1/demo/src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
+import Counter from './Components/Counter';
+import Product from './Components/Product';
+import Profile from './Components/Profile';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Counter/>
+      <Product/>
+      <Profile/>
+      <Todo/>
+      <Home/>
+    
     </div>
   );
 }
