79006994752abb139cc248b645a20d49f4a491a3
Abhishek6416 <abhishekkr6416@gmail.com>
2023-03-24 09:40:09 +0530

    routing

--- Routing-1/demo/src/App.js
+++ Routing-1/demo/src/App.js
@@ -4,16 +4,27 @@ import Navbar from './Components/Navbar';
 import Counter from './Components/Counter';
 import Product from './Components/Product';
 import Profile from './Components/Profile';
-
+import Home from './Components/Home';
+import Todo from './Components/Todo';
+import { BrowserRouter } from 'react-router-dom';
+import { Route,Routes } from 'react-router-dom';
+import PageNotFound from './Components/PageNotFound';
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <Counter/>
-      <Product/>
-      <Profile/>
-      <Todo/>
-      <Home/>
+      <Routes>
+      <Route path='/' element={<Home/>}/>
+
+      <Route path='/counter' element={<Counter/>}/>
+      <Route path='/product' element={<Product/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+      <Route path='/todo' element={<Todo/>}/>
+      <Route path='*' element={<PageNotFound/>}/>
+      
+        
+
+      </Routes>
     
     </div>
   );
---
+++ Routing-1/demo/src/Components/PageNotFound.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function PageNotFound() {
+
+    var divStyle={
+        color:"red",
+        textAlign:"center",
+        marginTop:"100px"
+    }
+  return (
+    <div style={divStyle}>
+
+        <h1>PAGE NO FOUND</h1>
+        <h2>enter corrcet url</h2>
+    </div>
+  )
+}
