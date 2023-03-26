2288ddb2aec393dab671b62420aa3aed248a0f0f
Abhishek6416 <abhishekkr6416@gmail.com>
2023-03-26 21:45:08 +0530

    routing

--- Routing-1/demo/src/Components/Navbar.js
+++ Routing-1/demo/src/Components/Navbar.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import "./Navbar.css"
-
+import { Link } from 'react-router-dom'
 export default function Navbar() {
   return (
     <div className='Nav'>
 
-        <a href="">Home</a>
-        <a href="">Profile</a>
-        <a href="">Todo</a>
-        <a href="">Products</a>
-        <a href="">Counter</a>
+        <Link to="/">Home</Link>
+        <Link to="/profile">Profile</Link>
+        <Link to="/todo">Todo</Link>
+        <Link to="/product">Products</Link>
+        <Link to="/counter">Counter</Link>
     </div>
   )
 }
