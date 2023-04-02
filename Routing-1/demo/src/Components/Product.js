554b5fda9c6de335906002c9481e61cec843edb1
Abhishek6416 <abhishekkr6416@gmail.com>
2023-04-02 18:12:32 +0530

    productpage

--- Routing-1/demo/src/Components/Product.js
+++ Routing-1/demo/src/Components/Product.js
@@ -1,11 +1,23 @@
 import React from 'react'
-
+import './Product.css'
+import { Link } from 'react-router-dom'
 export default function Product() {
   return (
-    <div>
+    <div className='main'>
 
-      <div>leftside</div>
-      <div>right</div>
+      <div className='main1'>
+
+        <Link to="">Product</Link>
+        <Link to="">Jewellery</Link>
+        <Link to="">yoyo</Link>
+        <Link to="">singh</Link>
+      </div>
+
+
+
+      <div>
+
+      </div>
     </div>
   )
 }
